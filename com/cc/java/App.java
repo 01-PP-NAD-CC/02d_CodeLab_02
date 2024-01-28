2184b23cec6847b7984589449af3d0e47a6cd5e0
nad1408 <155629463+nad1408@users.noreply.github.com>
2024-01-28 21:55:29 +0100

    Varriante 1 ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,12 +3,30 @@ package com.cc.java;
 public class App {
 
 	public static void main(String[] args) {
+	Mitarbeiter worker1 = new Mitarbeiter("Müller", "Tim", "Callcenter", 2010 );
+	Mitarbeiter worker2 = new Mitarbeiter("Schmitz", "Heiko", "Lieferant", 2005 );
+	Mitarbeiter worker3 = new Mitarbeiter("Schubert", "Max", "Manager", 1999 );
+
+	output(worker1.getInfo("familyName"));
+	output(worker1.getInfo("firstName"));
+	output(worker1.getInfo("role"));
+	output(worker1.getInfo("yearOfEntry"));
+
+	output(worker2.getInfo("familyName"));
+	output(worker2.getInfo("firstName"));
+	output(worker2.getInfo("role"));
+	output(worker2.getInfo("yearOfEntry"));
 	
-	}
+	output(worker3.getInfo("familyName"));
+	output(worker3.getInfo("firstName"));
+	output(worker3.getInfo("role"));
+	output(worker3.getInfo("yearOfEntry"));
+}
 
 
 	private static void output(String outStr) {
 		System.out.println(outStr);
 	}
 
-};
+}
+
--- com/cc/java/Mitarbeiter.java
+++ com/cc/java/Mitarbeiter.java
@@ -16,8 +16,19 @@ public class Mitarbeiter {
 
 
 	public String getInfo(String flag) {
-		// switch ..
-		return null;
+		switch (flag) {
+			case "familyName":
+					return this.familyName;
+			case "firstName":	
+					return this.firstName;
+			case "role":
+					return this.role;		
+			case "yearOfEntry":
+					return Integer.toString(this.yearOfEntry);  // geht auch : return String.valueOf(yearOfEntry)
+			default: 
+					return "error";		
+		}
+		
 	}
 
 }
